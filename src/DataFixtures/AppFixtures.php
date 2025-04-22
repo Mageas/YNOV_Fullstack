161b56bef620e8bb968aa0855991a070e329dc03
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2025-04-22 16:32:08 +0200

    feature: add custom fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,19 +2,32 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Song;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Faker\Provider\Goat;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create("fr_FR");
+        $this->faker->addProvider(new Goat($this->faker));
+    }
+
     public function load(ObjectManager $manager): void
     {
-        $song = new Song();
-        $song->setName("Fury");
-        $song->setArtiste("Muse");
+        foreach(range(1, 100) as $i) {
+            $song = new Song();
+            $song->setName($this->faker->goatName());
+            $song->setArtiste("Muse");
 
-        $manager->persist($song);
+            $manager->persist($song);
+        }
 
         $manager->flush();
     }
