c63b6f91bc49b5f8e049b5bbc9c9df00ead654ce
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2025-04-23 10:08:28 +0200

    feature: add controller create

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -4,11 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Song;
 use App\Repository\SongRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 final class SongController extends AbstractController
 {
@@ -21,11 +24,25 @@ final class SongController extends AbstractController
         return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
     }
 
-    #[Route('api/v1/song/{song}', name: 'get_song', methods: ['GET'])]
-    public function get(Song $song, SerializerInterface $serializer): JsonResponse
+    #[Route('api/v1/song/{id}', name: 'get_song', methods: ['GET'])]
+    public function get(Song $id, SerializerInterface $serializer): JsonResponse
     {
-        $jsonData = $serializer->serialize($song, 'json');
+        $jsonData = $serializer->serialize($id, 'json');
         return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('api/v1/song', name: 'create_song', methods: ['POST'])]
+    public function create(Request $request, UrlGeneratorInterface $urlGenerator, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $song = $serializer->deserialize($request->getContent(), Song::class, 'json');
+
+        $entityManager->persist($song);
+        $entityManager->flush();
+
+        $jsonData = $serializer->serialize($song, 'json');
+        $location = $urlGenerator->generate('get_song', ['id' => $song->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonData, Response::HTTP_CREATED, ['location' => $location], true);
     }
 
     // #[Route('api/v1/song/{song}', name: 'get_song', methods: ['GET'])]
