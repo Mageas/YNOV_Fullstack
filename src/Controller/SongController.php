4449fc2517fd8b1d5cab61a92d2a69314b8fd1cd
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2025-04-23 09:26:04 +0200

    feature: add controller get

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Song;
 use App\Repository\SongRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -19,4 +20,24 @@ final class SongController extends AbstractController
 
         return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
     }
+
+    #[Route('api/v1/song/{song}', name: 'get_song', methods: ['GET'])]
+    public function get(Song $song, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonData = $serializer->serialize($song, 'json');
+        return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
+    }
+
+    // #[Route('api/v1/song/{song}', name: 'get_song', methods: ['GET'])]
+    // public function get(Song $song, SongRepository $songRepository, SerializerInterface $serializer): JsonResponse
+    // {
+    //     // $data = $songRepository->findOneBy([
+    //     //     'id' => $song->getId(),
+    //     // ]);
+
+    //     $data = $songRepository->find($song);
+    //     $jsonData = $serializer->serialize($data, 'json');
+
+    //     return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
+    // }
 }
